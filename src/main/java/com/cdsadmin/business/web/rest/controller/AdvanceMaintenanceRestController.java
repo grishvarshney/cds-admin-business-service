618cff15406c32c4777e4fde131c34a993907395
User <User@DESKTOP-G66PANM.fios-router.home>
2020-01-15 16:46:47 -0500

    Cross Origin disabled

--- src/main/java/com/cdsadmin/business/web/rest/controller/AdvanceMaintenanceRestController.java
+++ src/main/java/com/cdsadmin/business/web/rest/controller/AdvanceMaintenanceRestController.java
@@ -3,6 +3,7 @@ package com.cdsadmin.business.web.rest.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -17,6 +18,7 @@ import com.cdsadmin.business.service.AdvanceMaintenanceService;
 
 @RestController
 @RequestMapping("/advancemaintenance")
+@CrossOrigin(origins = "*")
 public class AdvanceMaintenanceRestController {
 	
 	@Autowired
--- src/main/java/com/cdsadmin/business/web/rest/controller/TransferRestController.java
+++ src/main/java/com/cdsadmin/business/web/rest/controller/TransferRestController.java
@@ -4,15 +4,20 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.cdsadmin.business.domain.Note;
 import com.cdsadmin.business.domain.Transfer;
 import com.cdsadmin.business.service.TransferService;
 
+@RestController
+@RequestMapping("/transfer")
+@CrossOrigin(origins = "*")
 public class TransferRestController {
 	
 	@Autowired
